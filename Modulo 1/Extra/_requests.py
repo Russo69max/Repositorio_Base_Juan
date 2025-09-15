e7c8cc18e2364b0a668ffaed7108979ef59c6d6a
Russo69max <juanpedrofreitas69@gmail.com>
2025-09-15 11:21:02 -0300

    Add files via upload

---
+++ Modulo 1/Extra/_requests.py
@@ -0,0 +1,13 @@
+import requests
+
+try:
+    url = "https://sp.senai.br/unidade/santanadeparnaiba/"
+    resposta = requests.get(url)
+    repota = (resposta.status_code)
+    if repota == 200:
+        print("Está tudo correto | status:",repota)
+    else:
+        print("Erro não encontrado | status:",repota )
+except:
+    print("Ezequiel deu a bunda por isso nao funcionou ")
+
