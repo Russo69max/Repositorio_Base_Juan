5440edccfd1c57ee8de933e58c389e2a191956fd
Russo69max <juanpedrofreitas69@gmail.com>
2025-09-15 10:50:00 -0300

    Add files via upload

---
+++ Modulo 1/Loopings/Russo (4).py
@@ -0,0 +1,21 @@
+
+opçao = 1
+while opçao != 2:
+    nome =  input("Qual o seu nome? ")
+    peso = float(input("Qual é seu peso em quilos?"))
+    altura = float(input("Qual a sua altura?"))
+        
+    IMC = peso / (altura * altura)
+    opçao = input("Digite um numero \n 1-Calcular o imc \n 2-Sair do programa ")
+    if peso <= 18.5:
+        print(f"{nome} vc é um Frango")
+    elif IMC <= 24.9:
+        print(f"{nome} Está com peso normal")
+    elif IMC <= 29.9:
+        print(f"{nome} Está com Sobre peso")
+    elif IMC <= 34.9:
+        print(f"{nome} Está com obesidade Grau1!")
+    elif IMC <= 39.9:
+        print(f"{nome} Está com obesidade Grau2!")
+    else:
+        print(f"{nome} Para de comer doritos")
